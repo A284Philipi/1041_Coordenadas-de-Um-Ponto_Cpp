0bf5779e9df50b110f43e127bee2704dd0893ada
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-13 20:37:07 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cin >> y;
+    cin >> x;
+    if (x == 0 && y == 0){
+        cout << "Origem" <<endl;
+    }else{
+        if (x == 0 || y == 0){
+            if (x == 0){
+                cout << "Eixo X" <<endl;
+            }else{
+                cout << "Eixo Y" <<endl;
+            }
+        }else{
+            if (x > 0 && y > 0){
+                cout << "Q1" <<endl;
+            }else{
+                if (x > 0 && y < 0){
+                    cout << "Q2" <<endl;
+                }else{
+                    if (x < 0 && y < 0){
+                        cout << "Q3" <<endl;
+                    }else{
+                        cout << "Q4" <<endl;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
